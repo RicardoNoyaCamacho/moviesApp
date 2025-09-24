87b44d7506e1448969745a5616ebbb35249142c0
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2025-09-24 16:25:57 -0600

    add loader

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,17 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { useMovies } from '@/presentation/hooks/useMovies'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
 
-    const { nowPlayingQuery } = useMovies()
-    return (
-        <View>
-            <Text>HomeScreen</Text>
+    const safeArea = useSafeAreaInsets();
+    const { nowPlayingQuery } = useMovies();
 
-            <Text>
-                {JSON.stringify(nowPlayingQuery.data)}
-            </Text>
+    if (nowPlayingQuery.isLoading) {
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator color='purple' size={40} />
+
+            </View>
+        )
+    }
+
+    return (
+        <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+            <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
         </View>
     )
 }
